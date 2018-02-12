57b9dcb43460139c6a4ff0e00c20aff0680aa3d3
Huchro, Magdalena <magdalena.huchro@dieboldnixdorf.com>
2018-02-12 07:56:34 +0100

    solution for printer file

--- java-basic-traning-8/src/main/java/pl/codecouple/Printer.java
+++ java-basic-traning-8/src/main/java/pl/codecouple/Printer.java
@@ -5,11 +5,28 @@ class Printer {
     private StringBuilder stringBuilder;
 
     Printer(String text) {
-        this.stringBuilder = new StringBuilder(text);
+        if (text == null || text.isEmpty()) {
+            throw new IllegalArgumentException("Zloooooooo!");
+        } else {
+            this.stringBuilder = new StringBuilder(text);
+        }
     }
 
     public String reverseString() {
         return stringBuilder.reverse().toString();
     }
+
+    public int indexA() {
+        return stringBuilder.indexOf("a");
+    }
+
+    public String deleteA() {
+        return stringBuilder.deleteCharAt(0).toString();
+    }
+
+    public int indexC() {
+        return stringBuilder.lastIndexOf("c");
+    }
+
 
 }
--- java-basic-traning-8/src/test/java/pl/codecouple/PrinterSpec.groovy
+++ java-basic-traning-8/src/test/java/pl/codecouple/PrinterSpec.groovy
@@ -7,26 +7,51 @@ class PrinterSpec extends Specification {
 
     Should "return reverse string"() {
         given:
-            Printer printer = new Printer('abc')
+        Printer printer = new Printer('abc')
         when:
-            def result = printer.reverseString()
+        def result = printer.reverseString()
         then:
-            result == 'cba'
+        result == 'cba'
     }
 
     Should "return indexOf 'a' char from 'abc'"() {
+        given:
+        Printer printer = new Printer('abc')
+        when:
+        def result = printer.indexA()
+        then:
+        result == 0
 
     }
 
     Should "delete 'a' from 'abc'"() {
+        given:
+        Printer printer = new Printer('abc')
+        when:
+        def result = printer.deleteA()
+        then:
+        result == 'bc'
 
     }
 
-    Should "thrown IllegalArgumentException when given String in constructor is null or empty" () {
+    Should "thrown IllegalArgumentException when given String in constructor is null or empty"() {
+        given:
+        Printer printer = new Printer("")
+        when:
+        def result = printer.reverseString()
+        then:
+        result == null
+
 
     }
 
     Should "return lastIndexOf 'c' char from 'abc'"() {
+        given:
+        Printer printer = new Printer('abc')
+        when:
+        def result = printer.indexC()
+        then:
+        result == 2
 
     }
 
